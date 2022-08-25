7a500c6ead64d361c6f56cb7ccb45eccc42450a9
Tanner Denti <tannerwdenti@gmail.com>
2022-08-24 19:52:33 -0600

    Added two scenes, late first commit with lots of additions.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float timeRange = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRange -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeRange <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeRange = 1.0f;
+        }
+    }
+
+    
+
+}
